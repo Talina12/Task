fc82f3ce8d128cf915c817dc5cad9c6cb1d17121
dina <dina304@gmail.com>
2020-04-12 21:01:11 +0300

    remove findByToken only sql

--- src/main/java/com/food4good/database/repositories/UsersRepository.java
+++ src/main/java/com/food4good/database/repositories/UsersRepository.java
@@ -14,8 +14,6 @@ public interface UsersRepository extends JpaRepository<User, Long> {
 
 	Optional<User> findById(Long id);
     List<User> findAll();
-    Optional<User> findByName(String  name);
-    Optional<User> findByToken(String  token);
     Optional<User> findByTokenAndRoles(String  token,String roles);
     Optional<User> findByEmailAndRoles(String email, String roles);
     Optional<User> findByEmailAndPasswordAndRoles(String email, String password, String roles);
