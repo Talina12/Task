1a9ee0ae6d18e6785a92bd841d9869058ffe2954
Talina12 <talina.in@gmail.com>
2021-06-04 15:05:27 +0300

    field validator added

--- src/main/java/org/Dudnik/database/repositories/UserRepository.java
+++ src/main/java/org/Dudnik/database/repositories/UserRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-//I know to take action to prevent SQL injection, but I have not found how to do this when dealing with hibernate repositories
+
 public interface UserRepository extends CrudRepository<User, Integer> {
     Optional<User> findByEmail(String email);
 }
